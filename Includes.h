df559fdf136bcc34447b07f1d798df8b6b41ea75
Markus-Zacharuk <57815046+Markus-Zacharuk@users.noreply.github.com>
2023-12-26 14:13:58 +0100

    Create Includes.h

---
+++ Includes.h
@@ -0,0 +1,19 @@
+#ifndef INCLUDES_H
+#define INCLUDES_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <unordered_map>
+#include <unordered_set>
+
+#include <stdint.h>
+#include <optional>
+#include <memory>
+#include <boost/variant.hpp>
+#include <boost/variant/multivisitors.hpp>
+#include <boost/hana.hpp>
+#include <boost/variant/get.hpp>
+
+#endif
